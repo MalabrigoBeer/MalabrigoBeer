93ffbea49e4b330b9ac8329d56aa38ac8445c08f
Ivan Aguilar <anstechnological@gmail.com>
2022-08-20 10:07:51 -0300

    alla

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export default function Spotify(props) {
     const {
-      iframeSource = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0pq24pxXAUbInI6eHRGFRk?utm_source=generator" class="spotify" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+      iframeSource = '<iframe style="border-radius:12px" src="https://open.spotify.com/playlist/0tMgMS8DcKqIUTtHU7ylxt?si=ce249bb734924e42" class="spotify" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
     } = props;
     return <div className="spotify2" dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
   }
