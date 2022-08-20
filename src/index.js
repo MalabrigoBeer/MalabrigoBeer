a8d421c8444bd0709cc851d9946af064c819ac4d
Ivan Aguilar <anstechnological@gmail.com>
2022-08-20 17:29:12 -0300

    testiando

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css"
 
-const iframe = document.createElement('iframe');
-iframe.setAttribute('allow', 'fullscreen'); // must be 1st
-iframe.setAttribute('allowFullScreen', '');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export default function Spotify(props) {
     const {
-      iframeSource = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/0pq24pxXAUbInI6eHRGFRk?utm_source=generator" class="spotify" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
+      iframeSource = '<iframe class="spotify" src="https://open.spotify.com/embed/episode/7GAmzSosYFIPMRdMNa16xV?utm_source=generator" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
     } = props;
-    return <div className="spotify2" dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
+    return <div className="spotify" dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
   }
