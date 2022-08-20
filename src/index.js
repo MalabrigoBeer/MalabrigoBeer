5e88d09c7c4874fa879b4905c6cc115cfd178725
Mauricio Aguilar <mauricio.aguilar@incluit.com>
2022-08-20 18:22:27 -0300

    iframe

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css"
 
+const iframe = document.createElement('iframe');
+iframe.setAttribute('allow', 'fullscreen'); // must be 1st
+iframe.setAttribute('allowFullScreen', '');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
